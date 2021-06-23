22b295ff89a56300abcb5197732bfbfe4de3db61
Zobee <zohebboga@hotmail.com>
2021-06-23 19:43:51 +0000

    Added product, category, and inventory counts to the admin/dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["HTTP_AUTH_USER"], password: ENV["HTTP_AUTH_PW"]
   def show
+    @num_products = Product.count
+    @num_products_in_stock = Product.where("quantity >?", 0).count
+    @num_total_products = Product.sum("quantity")
+    @num_categories = Category.count
   end
 end
