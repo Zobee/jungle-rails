dd59456e6d90a7aebbb808b7affc5fe77cc1e168
Zobee <zohebboga@hotmail.com>
2021-06-24 17:29:37 +0000

    Added signup and login routes, models, and controllers

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = user.find_by_email(params[:email])
+    #If user exists && pw is correct
+    if user && user.authenticate(params[:password])
+      #Save uid inside cookie
+      session[:user_id] = user_id
+      redirect_to '/'
+    else
+      redirect_to '/login'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to '/login'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  get "/about", to: "about#index"
   root to: 'products#index'
+
+  get "/about", to: "about#index"
+  get '/signup' => 'users#new'
+  post '/users' => 'users#create'
+  
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
 
   resource :cart, only: [:show] do
     post   :add_item
