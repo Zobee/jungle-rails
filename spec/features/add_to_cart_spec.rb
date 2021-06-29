5459a123a2acc1c9c5597425930109dc0a223cc3
Zobee <zohebboga@hotmail.com>
2021-06-29 14:14:30 +0000

    Implemented an 'add to cart' spec to ensure that user's cart is updated when a user adds an item to their cart

--- spec/features/add_to_cart_spec.rb
+++ spec/features/add_to_cart_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
-RSpec.feature "AddToCarts", type: :feature do
-  pending "add some scenarios (or delete) #{__FILE__}"
+RSpec.feature "AddToCart", type: :feature, js: true do
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    5.times do |n|
+      @category.products.create!(
+      name:  Faker::Hipster.sentence(3),
+      description: Faker::Hipster.paragraph(4),
+      image: open_asset('apparel1.jpg'),
+      quantity: 10,
+      price: 64.99
+      )
+    end
+  end
+  scenario "User can click the 'Add' button on a product to add item to their cart" do
+    # ACT
+    visit root_path
+    expect(page).to have_text 'My Cart (0)'
+    within(page.all('article.product').first) do
+      click_on 'Add'
+    end
+    # DEBUG / VERIFY
+    expect(page).to have_text 'My Cart (1)'
+    save_screenshot
+  end
 end
