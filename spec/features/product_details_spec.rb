51bfa8302c9553a273b50bc5afa153832f9e20e8
Zobee <zohebboga@hotmail.com>
2021-06-29 14:05:49 +0000

    More cleanup for product_details_spec

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -2,21 +2,10 @@ require 'rails_helper'
 
 RSpec.feature "ProductDetails", type: :feature, js: true do
   before :each do
-    @main_category = Category.create! name: 'Apparel'
-    @unrelated_category = Category.create! name: 'Home'
+    @category = Category.create! name: 'Apparel'
 
     5.times do |n|
-      @main_category.products.create!(
-      name:  Faker::Hipster.sentence(3),
-      description: Faker::Hipster.paragraph(4),
-      image: open_asset('apparel1.jpg'),
-      quantity: 10,
-      price: 64.99
-      )
-    end
-
-    5.times do |n|
-      @unrelated_category.products.create!(
+      @category.products.create!(
       name:  Faker::Hipster.sentence(3),
       description: Faker::Hipster.paragraph(4),
       image: open_asset('apparel1.jpg'),
